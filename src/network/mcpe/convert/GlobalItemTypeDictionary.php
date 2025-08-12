4d324c2baa655cc7db08f059c7b205f25a3294bb
Benedikt05 <60869945+Benedikt05@users.noreply.github.com>
2025-08-12 21:43:38 +0200

    ...

--- src/network/mcpe/convert/GlobalItemTypeDictionary.php
+++ src/network/mcpe/convert/GlobalItemTypeDictionary.php
@@ -45,7 +45,7 @@ final class GlobalItemTypeDictionary{
 	use SingletonTrait;
 
 	private static function make() : self{
-		$data = Filesystem::fileGetContents(BedrockDataFiles::REQUIRED_ITEM_LIST_JSON));
+		$data = Filesystem::fileGetContents(BedrockDataFiles::REQUIRED_ITEM_LIST_JSON);
 		$table = json_decode($data, true);
 		if(!is_array($table)){
 			throw new AssumptionFailedError("Invalid item list format");
