96c4b87b96eea1dc431d5f2523d5ebcab4f4c068
Benedikt05 <60869945+Benedikt05@users.noreply.github.com>
2025-12-10 01:27:06 +0100

    1.21.130

--- src/network/mcpe/handler/SessionStartPacketHandler.php
+++ src/network/mcpe/handler/SessionStartPacketHandler.php
@@ -71,7 +71,6 @@ final class SessionStartPacketHandler extends PacketHandler{
 	}
 
 	protected function isCompatibleProtocol(int $protocolVersion) : bool{
-		return $protocolVersion === ProtocolInfo::CURRENT_PROTOCOL ||
-			(ProtocolInfo::CURRENT_PROTOCOL === 859 && $protocolVersion === 860);
+		return $protocolVersion === ProtocolInfo::CURRENT_PROTOCOL;
 	}
 }
